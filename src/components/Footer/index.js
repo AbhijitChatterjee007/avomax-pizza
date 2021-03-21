ab338306e1f1dd1b204eef69b3c5a7d969c85d69
AbhijitChatterjee007 <abhijit.chatterjee.5200@gmail.com>
2021-03-22 00:49:42 +0530

    updating social handles

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -24,24 +24,10 @@ const Footer = () => {
           <SocialMediaWrap>
             <SocialLogo to='/'>Pizza</SocialLogo>
             <SocialIcons>
-              <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
-                <FaFacebook />
-              </SocialIconLink>
-              <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+              <SocialIconLink href='https://www.instagram.com/abhijitchatterjee_/' target='_blank' aria-label='Instagram'>
                 <FaInstagram />
               </SocialIconLink>
-              <SocialIconLink href='/' target='_blank' aria-label='Youtube'>
-                <FaYoutube />
-              </SocialIconLink>
-              <SocialIconLink
-                href='//www.twitter.com/briandesignz'
-                target='_blank'
-                aria-label='Twitter'
-                rel='noopener noreferrer'
-              >
-                <FaTwitter />
-              </SocialIconLink>
-              <SocialIconLink href='/' target='_blank' aria-label='Linkedin'>
+              <SocialIconLink href='https://www.linkedin.com/in/abhijitchatterjee007/' target='_blank' aria-label='Linkedin'>
                 <FaLinkedin />
               </SocialIconLink>
             </SocialIcons>
